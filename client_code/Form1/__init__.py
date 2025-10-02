0ec3c2e546fc9628b48e6ff2da9de621f45a79cf
tobi_kellner@yahoo.com <tobi_kellner@yahoo.com>
2025-10-02 22:39:19 +0000

    Created ServerModule1; Edited settings, Form1 and custom package dependencies

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,7 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import plotly.graph_objects as go
 
 
 class Form1(Form1Template):
@@ -8,3 +10,24 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def file_loader_1_change(self, file, **event_args):
+    if file:
+      result = anvil.server.call('process_file', file)
+      self.plot_heatmap(result)
+
+  def text_area_1_change(self, **event_args):
+    text_data = self.text_area_1.text
+    if text_data.strip():
+      result = anvil.server.call('process_text', text_data)
+      self.plot_heatmap(result)
+
+  def plot_heatmap(self, result):
+    # result contains: x (dates), y (times), z (values)
+    fig = go.Figure(data=go.Heatmap(
+      x=result['x'],
+      y=result['y'],
+      z=result['z'],
+      colorscale='Viridis'
+    ))
+    self.plot_1.figure = fig
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,34 @@
+import anvil.server
+import pandas as pd
+import io
+
+@anvil.server.callable
+def process_file(file):
+  # Read CSV into pandas
+  df = pd.read_csv(io.BytesIO(file.get_bytes()))
+  return _process_dataframe(df)
+
+@anvil.server.callable
+def process_text(text_data):
+  # Assume tab or comma separated
+  df = pd.read_csv(io.StringIO(text_data), sep=None, engine="python")
+  return _process_dataframe(df)
+
+def _process_dataframe(df):
+  # Expect two columns: datetime, value
+  df.columns = ['datetime', 'value']
+  df['datetime'] = pd.to_datetime(df['datetime'])
+
+  # Extract date and time-of-day
+  df['date'] = df['datetime'].dt.date
+  df['time'] = df['datetime'].dt.strftime('%H:%M')
+
+  # Pivot into matrix for heatmap
+  pivot = df.pivot_table(index='time', columns='date', values='value', aggfunc='mean')
+
+  return {
+    'x': list(pivot.columns.astype(str)),   # dates
+    'y': list(pivot.index),                 # times
+    'z': pivot.values.tolist()              # matrix of values
+  }
+
